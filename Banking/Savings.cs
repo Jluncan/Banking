5d37f701c69b427c8658b306338b72b7576707a1
James Luncan <jluncan@tql.com>
2024-06-18 10:19:37 -0400

    Built savings using composition

--- Banking/Savings.cs
+++ Banking/Savings.cs
@@ -5,18 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Banking {
-    internal class Savings : Account {
+    internal class Savings {
+
+        private Account account = new Account("Savings Account");
+
         public decimal IntRate { get; set; } = 0.12m;
-        
-        public void CalcAndPayInterest(int NbrOfMonths) {
-            decimal Interest = Balance * (IntRate / 12) * NbrOfMonths;
+
+        public void CalcInterestAndDeposit(int months) {
+        var Interest = Balance() * (IntRate / 12) * months; 
             Deposit(Interest);
         }
 
-        public Savings(string description) : base(description) {
+        public int AccountId() {
+            return account.AccountId;
         }
 
+        public string Description() {
+            return account.Description;
 
+        }
+        public void Description(string description) {
+            account.Description = description;
+        }
+
+        public decimal Balance() {
+            return account.Balance;
+        }
+
+        public bool Deposit(decimal Amount) {
+            return account.Deposit(Amount);
+
+        }
+
+        public bool Withdraw(decimal Amount) {
+            return account.Withdraw(Amount);
+        }
+
+        public bool Transfer(decimal Amount, Savings Savings) {
+            if (account.Withdraw(Amount)) {
+                Savings.Deposit(Amount);
+            }
+            return true;
+
+        }
+        public Savings(string description) {
+        }
     }
-
 }
